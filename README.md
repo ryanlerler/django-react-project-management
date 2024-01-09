8eb518db451a4dfcbc9ac55041d1e368d6d448a3
ryanlerler <lyp2726@outlook.com>
2024-01-09 21:11:14 +0800

    Set up react & cors

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # django-react-project-management
+
+Follow along tutorial @ 
+https://www.youtube.com/watch?v=wCgQjXI1Q0w&list=PLmEKHA8iFrmBCo1Guf3xbM1af5p5Ja-fy&index=2&ab_channel=CBIAnalytics
--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'cors-headers'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,6 +39,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+  'http://127.0.0.1:5173/'
 ]
 
 ROOT_URLCONF = 'crud.urls'
