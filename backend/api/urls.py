82aeae5bb4d0a5e839a609a24a88a6feb4b6246b
ryanlerler <lyp2726@outlook.com>
2024-06-09 12:20:24 +0800

    Complete crud

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import *
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-  path('', home)
-]
+router = DefaultRouter()
+router.register('project', ProjectViewSet, 'project')
+
+urlpatterns = router.urls
